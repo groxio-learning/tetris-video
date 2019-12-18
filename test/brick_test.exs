6b22c1b00362cd87d81b9d59a3ee3ef09a609790
Bruce Tate <bruce@rapidred.com>
2019-12-18 14:30:41 -0500

    tests for new_random

--- test/brick_test.exs
+++ test/brick_test.exs
@@ -7,5 +7,13 @@ defmodule BrickTest do
     assert new_brick().name == :i
   end
   
+  test "Creates a new random brick" do
+    actual = new_random()
+    
+    assert actual.name in [:i, :l, :z, :t, :o]
+    assert actual.rotation in [0, 90, 180, 270]
+    assert actual.reflection in [true, false]
+  end
+  
   def new_brick, do: new()
 end
