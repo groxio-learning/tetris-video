7c38e0da6c3ebe0e8a1f3b8d2500baba6ffc48c9
Pierre-Alexandre Piarulli <go@rubyrock.me>
2020-01-12 11:19:52 +0100

    remove trailling space at the end of line

--- test/brick_test.exs
+++ test/brick_test.exs
@@ -1,11 +1,11 @@
 defmodule BrickTest do
   use ExUnit.Case
-  
+
   import Tetris.Brick
 
   test "Creates a new brick" do
     assert new_brick().name == :i
   end
-  
+
   def new_brick, do: new()
 end
